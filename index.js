a2e87eca48a411815933a8c12dcb21e9b8d55570
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-07-15 17:39:44 -0300

    cookies

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 const path = require("path")
 const app = express()
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "main.html"))
 })
 app.listen(3003)
 app.post("/api", async (req, res) => {
-    console.log(req)
+    console.log(req.cookies)
+    res.cookie('username', 'JohnDoe', {
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        path: '/api',
+        secure: true,
+        httpOnly: true,
+        sameSite: 'Strict'
+      })
     res.status(200)
 })
